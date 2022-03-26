4e9cd58a75ea835e60abc149e8394cbf2cb692c6
Rocky Sinha <60058512+Rocky-Sinha@users.noreply.github.com>
2022-03-26 09:25:56 +0600

    line added

--- project-imp-1.cpp
+++ project-imp-1.cpp
@@ -14,6 +14,8 @@
 /* again some new lines are added*/
 /*new line added from github ***/
 
+/*line added from rocky*/
+
 void init(void){
     glClearColor(0,0,0,0); //GLfloat red,green,blue,alpha initial value 0 alpha values used by glclear to clear the color buffers
     glMatrixMode(GL_PROJECTION);  // To specify which matrix is the current matrix & projection applies subsequent matrix to projecton matrix stack
